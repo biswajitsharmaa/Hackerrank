3e9997cddcd8586cf06e9435feef1da90d6c3c49
biswajitsharmaa <biswajitmungpoo503@gmail.com>
2018-10-23 16:02:18 +0530

    Program to calculate the no of pairs in the array

---
+++ sockmerchant.c
@@ -0,0 +1,38 @@
+// Complete the sockMerchant function below.
+int sockMerchant(int n, int ar_count, int* ar) 
+{
+   
+ 
+        int i, j,a,count=0;
+ 
+        for (i = 0; i < n; ++i) 
+        {
+ 
+            for (j = i + 1; j < n; ++j)
+            {
+ 
+                if (ar[i] > ar[j]) 
+                {
+ 
+                    a =  ar[i];
+                    ar[i] = ar[j];
+                    ar[j] = a;
+ 
+                }
+ 
+            }
+ 
+        }
+    
+        for( i=0;i<n-1;i++){
+            if(ar[i]==ar[i+1])
+            {
+                count=count+1;
+                i=i+1;
+            }
+        }
+        return count;
+    
+
+
+}
